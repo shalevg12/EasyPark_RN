4ebc45b7d7f9c332dc7a41b1a79773f141134561
shalevg12 <58697095+shalevg12@users.noreply.github.com>
2022-12-21 17:11:20 +0200

    Add files via upload

---
+++ assets/index.js
@@ -0,0 +1,6 @@
+export { default as HeroImage } from "./hero2.png";
+export { default as Avatar } from "./avatar.png";
+export { default as Hotels } from "./carLogo.png";
+export { default as Attractions } from "./messages.png";
+export { default as Restaurants } from "./finance.png";
+export { default as NotFound } from "./NotFound.png";
